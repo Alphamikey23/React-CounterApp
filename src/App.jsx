dd4851627affeecd7c676289bc9c3f3ba6305f82
Jay <eng22cse001@gmail.com>
2024-06-21 14:00:05 +0530

    updated react

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//    <div>
+//     <h1> Counter App</h1>
+//     <button>Increment</button>
+//     <button>Decrement</button>
+//     <button>Reset</button>
+//    </div>
+//   )
+// }
+
+// class counter extends React.Component {
+//   render() {
+//     return 
+//     <div>
+//       <h2> Counter App</h2>
+//       </div>
+//   }
+// }
+
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+
+// class Counter extends React.Component {
+//   render() {
+//     return <h2>Counter App</h2>;
+//   }
+// }
+
+// const container = document.getElementById('root');
+// const root = ReactDOM.createRoot(container);
+// root.render(<Counter />);
+
+              
+
+
+// export default App
+
+import React from 'react';
+import Counter from './Counter';
+
+function App() {
+  return (
+    <div className="App">
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
